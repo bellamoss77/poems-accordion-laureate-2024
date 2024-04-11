8b7d3b8a40436718ea7d77e2ed2bb0222e5bf3ad
Maggie <mmf295@icloud.com>
2024-04-11 11:49:41 -0400

    add Lightbox and ImageGallery component files, add visual art images to public folder

---
+++ src/imageGallery.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import Lightbox from './Lightbox';
+
+const ImageGallery = () => {
+    const [selectedImage, setSelectedImage] = useState(null);
+    
+    const images = [
+        {
+            src: './images_gallery/Catherine_Selfridge-square.png',
+            
+        }
+    ]
+}
